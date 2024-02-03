2e3bb7d0feccbab490bfb87e37bb90740921e8a0
latlante93 <antoine_messager@yahoo.fr>
2024-02-03 13:42:50 +0100

    wip

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,43 +1,24 @@
-import mysql.connector
+import pymysql
 
-# Définir les informations de connexion à la base de données
-config = {
-    'user': 'if0_35903117',
-    'password': 'CaSSeTTe8963',
-    'host': 'sql113.byetcluster.com',
-    'database': 'if0_35903117_vocable',
-    'raise_on_warnings': True
-}
-print(config)
-# Créer une connexion à la base de données
+timeout = 10
+connection = pymysql.connect(
+  charset="utf8mb4",
+  connect_timeout=timeout,
+  cursorclass=pymysql.cursors.DictCursor,
+  db="defaultdb",
+  host="vocable-vocable.a.aivencloud.com",
+  password="AVNS_PuVJXwhe3IbdGZFJG_k",
+  read_timeout=timeout,
+  port=27109,
+  user="avnadmin",
+  write_timeout=timeout,
+)
+  
 try:
-    connection = mysql.connector.connect(**config)
-
-    if connection.is_connected():
-        print("Connexion réussie à la base de données")
-
-        # Créer un objet curseur pour exécuter des requêtes SQL
-        cursor = connection.cursor()
-
-        # Spécifier la table que vous souhaitez interroger
-        table_name = 'nouns'
-
-        # Exécuter une requête SQL pour récupérer la première ligne de la table
-        query = f"SELECT * FROM {table_name} LIMIT 1"
-        cursor.execute(query)
-
-        # Récupérer la première ligne et l'imprimer
-        first_row = cursor.fetchone()
-        print("Première ligne de la table:")
-        print(first_row)
-
-except mysql.connector.Error as err:
-    print(f"Erreur: {err}")
-
+  cursor = connection.cursor()
+  cursor.execute("CREATE TABLE mytest (id INTEGER PRIMARY KEY)")
+  cursor.execute("INSERT INTO mytest (id) VALUES (1), (2)")
+  cursor.execute("SELECT * FROM mytest")
+  print(cursor.fetchall())
 finally:
-    # Fermer le curseur et la connexion, même en cas d'erreur
-    if 'cursor' in locals() and cursor is not None:
-        cursor.close()
-    if 'connection' in locals() and connection.is_connected():
-        connection.close()
-        print("Connexion à la base de données fermée")
+  connection.close()
