a3d2190bd970d296557a4648a9e24c7f7d271834
antoine <antoine_messager@yahoo.fr>
2024-09-17 16:50:27 -0500

    test

--- test.py
+++ test.py
@@ -1,15 +1,18 @@
 from functions import *
+from tqdm import tqdm
 
-df=run(
-    """CREATE TABLE verb_es_fr AS (
-        verb_id integer(5), 
-        verb_fr varchar(255),
-        verb_es varchar(255),
-        present varchar(255),
-        futur varchar(255),
-        passe varchar(255),
-        conditionnel varchar(255),
-        PRIMARY KEY (verb_id, verb_fr)
-    );
-    """)
+data=pd.read_csv('Data/english-learning.csv')
+for i in tqdm(range(data.shape[0])):
+    id=data['rank'].values[i]
+    es=data.word_es.values[i]
+    en=data.word_en.values[i]
+
+    instruction=f"""
+    INSERT INTO es_to_en_data (word_id, word_es, word_en) 
+    VALUES ({id}, '{es}', '{en}');
+    """
+instruction="select * from es_to_en_data"
+df=run(instruction)
 print(df)
+
+
