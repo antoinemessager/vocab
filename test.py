22c49929754b65bb00bc2ff443c6423d652ccdf4
antoine <antoine_messager@yahoo.fr>
2024-09-26 14:08:01 -0500

    wip

--- test.py
+++ test.py
@@ -1,11 +1,17 @@
 from functions import *
 from tqdm import tqdm
 
-instruction="""
-INSERT INTO es_to_en_users (user_id, user_name) 
-VALUES (1, 'Antoine');
-"""
-df=run(instruction)
-print(df)
+instruction=f"""
+    CREATE TABLE es_to_en_history_user_1 (
+        word_id integer(5) not null, 
+        box_level integer(5) not null,
+        ts datetime not null,
+        PRIMARY KEY(word_id, ts)
+    )"""
+
+df=pd.read_csv('Data/english-learning.csv')
+df['rank']=df.index.astype(int)+1
+df.to_csv('Data/english-learning.csv',index=False,header=True)
+
 
 
